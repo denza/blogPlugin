c492e7c26b6e7ee1b37cd9ed5554e3674dd3a245
denza <besicdenis@gmail.com>
2019-06-30 02:48:24 +0200

    Blog dummy site

--- src/widget/containers/Widget.js
+++ src/widget/containers/Widget.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import { hot } from 'react-hot-loader/root';
 
+import Posts from '../components/Posts';
+import PostForm from '../components/PostForm';
+
 const Widget = () => (
-  <h1>Widget - Hello World!</h1>
+  <div>
+    <PostForm />
+    <hr />
+    <Posts />
+  </div>
 );
 
 export default hot(Widget);
